9a831d63d916cc950c96208d298d5705fff5cd35
Emil <isip_e.t.hodjakhov@mpt.ru>
2024-05-15 15:48:34 +0300

    Логика для интерфейса

---
+++ ClientServer/TcpClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prct7csharp.ClientServer
+{
+    internal class TcpClient
+    {
+    }
+}
